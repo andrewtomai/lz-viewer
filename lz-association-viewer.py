098dd79d9a5dabf53d0c870f05cf0a98c661b9d0
atomai <atomai@umich.edu>
2016-05-06 11:04:33 -0400

    Implementing my own solution for reading tabix data

--- lz-association-viewer.py
+++ lz-association-viewer.py
@@ -4,7 +4,9 @@
 import pysam
 import argparse
 import json
-from flask import Flask, jsonify
+import subprocess
+import re 
+from flask import Flask, jsonify, request, render_template, url_for
 
 ##REQUIRES: nothing
 ##MODIFIES: file_name, port_number
@@ -15,7 +17,7 @@ def check_options():
 	#add the port argument
 	parser.add_argument("-p", "--port", help="Specify a port at which to view results", type=int)
 	#add the filename argument
-	parser.add_argument("filename", type=str, help="Provide name of the file to be jsonified")
+	parser.add_argument("filename", type=str, help="Provide the name of the file to be jsonified")
 	#parse the arguments
 	args = parser.parse_args()
 	#set the filename
@@ -30,14 +32,114 @@ def check_options():
 	return {'filename' : file, 'port_number' : port_number }
 
 
+##REQUIRES filename is a tabix file
+##MODIFIES stdout
+##EFFECTS reads the header of filename using terminal command "tabix -H", returns a list of names.
+def check_header(filename):
+	#specify the command to be sent to stdout
+	command = "tabix " + filename + " -H"
+	#send command to stdout
+	p =  subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	#take in the text that was output
+	text = p.stdout.read()
+	retcode = p.wait()
+	#split this text into words
+	words = text.split()
+	#initialize a list of names of columns
+	names = []
+	#loops through the columns to create list of names
+	for word in words:
+		if word == "BEGIN":
+			names.append("position")
+		elif word == "MARKER_ID":
+			names.append("variant")
+		else: 
+			names.append(word.lower())
+	return names
 
 
+
+
+######################################################################################################
+#########--------------------------MY SOLUTION FOR TABIX-----------------------------#################
+##REQUIRES filename is a tabix file, names is the list of header info from the file
+##MODIFIES stdout
+##EFFECTS reads the data of filename using the terminal command "tabix [range]", returns a dictionary of data
+def gather_data(filename, names):
+	#initialize the dictionary with proper keys
+	data_dict = { }
+	for name in names:
+		data_dict[name] = []
+
+
+	#specify the command to be sent to stdout
+	command = "tabix " + filename + " 11:193154-193775"	
+	#send to stdout, and take in the text that was output
+	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	text = p.stdout.read()
+	retcode = p.wait()
+
+
+	#get the number of columns
+	num_columns = len(names)
+	#split the data into datum
+	data = text.split()
+	#initialize the current column
+	current_column = 0
+
+	#gather the data
+	for datum in data:
+		if current_column == num_columns:
+			current_column = 0
+		if datum == '\t':
+			continue
+		elif datum == '\n':
+			continue
+
+		#add the datum to the correct key using 'add_datum'
+		data_dict = add_datum(names, current_column, datum, data_dict)
+		current_column += 1
+	return data_dict
+
+
+
+
+##REQUIRES names is header list, current_column is < len(names), dict has keys from names
+##MODIFIES dict
+##EFFECTS adds 'datum' to the dict
+def add_datum(names, column, datum, dict):
+	
+	if datum == "NA":
+		dict[names[column]].append(None)
+	elif re.search('[a-zA-Z]', datum) != None:
+		#then its the name of a variant
+		dict[names[column]].append(datum)
+	elif "." in datum:
+		#then its a float
+		dict[names[column]].append(float(datum))
+	else:
+		#it must be a long int or an int
+		dict[names[column]].append(long(datum))
+	return dict
+		
+
+#############----------------------------------------------------------------------##############
+#################################################################################################
+
+
+
+
+
+
+
+#################################################################################################
+#############-------------------PYSAM SOLUTION FOR TABIX---------------------####################
 ##REQUIRES: file_name is a TABIX file
 ##MODIFIES: file
 ##EFFECTS: opens the TABIX formatted file using pysam 
-def open_file(file_name):
+def open_file(filename):
 	#open the tabix file
-	file = pysam.TabixFile(file_name)
+	file = pysam.TabixFile(filename)
 	return file
 
 
@@ -59,16 +161,22 @@ def create_data(file):
 		words = row.split()
 		#adding the data to the dictionary
 		data[variant].append(words[3])
-		if words[8] == 'NA': data[pvalue].append(words[8])
+		if words[8] == 'NA': data[pvalue].append(None)
 		else: data[pvalue].append(float(words[8]))
 
 		data[position].append(long(words[1]))
 		
 	return data
+##################---------------------------------------------------------------#################
+##################################################################################################
+
+
+
+
 
 ##REQUIRES data is a dictionary 
 ##MODIFIES data
-##EFFECTS adds necessary 'lastPage' key  
+##EFFECTS adds necessary 'lastPage' key to the dictionary  
 def format_data(data):
 	new_dict = {'data' : data, 'lastPage' : None}
 	return new_dict
@@ -76,10 +184,10 @@ def format_data(data):
 
 
 ##Flask initialization	
-lz_app = Flask(__name__)
+lz_app = Flask(__name__, static_url_path='')
 @lz_app.route('/')
 def home():
-	return "hello world!"
+	return lz_app.send_static_file('lz-association-viewer.html')
 @lz_app.route('/api', methods=['GET'])
 ##REQUIRES object is a dictionary
 ##MODIFIES lz_app
@@ -89,16 +197,14 @@ def api():
 	arguments = check_options()
 	filename = arguments["filename"]
 	
-	#open the specified file
-	file = open_file(filename)
-
-	#create the dictionary from file
-	data = create_data(file)
+	#get the header of the file
+	header = check_header(filename)
+	data = gather_data(filename, header)
 
 	#format the dictionary according to the portal API
 	object = format_data(data)
 
-	return jsonify(object)
+	return json.dumps(object)
 
 #----------------------------------------------------------------------------------------------------#
 ######################################################################################################
@@ -108,16 +214,8 @@ if __name__ == '__main__':
 
 	#check the input arguments
 	arguments = check_options()
-	#filename = arguments["filename"]
-	port_number = arguments["port_number"]
 
-	#open the specified file
-	#file = open_file(filename)
-	#create the dictionary from file
-	#data = create_data(file)
-	#format the dictionary according to the portal API
-	#object = format_data(data)
-	#print object
+	port_number = arguments["port_number"]
 
 	#run the flask webserver
 	lz_app.run(port = port_number)
